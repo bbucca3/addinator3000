b70ad88484da9a2cc4a6e4dbd8cdd0a39102a963
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-04-16 01:27:41 -0400

    flesh out the skeleton

--- app.js
+++ app.js
@@ -4,18 +4,34 @@ class ViewManager {
 
 	connectEventHandlers() {
 		// wire up event handler for form submit
+		document.getElementById('form-numbers')
+			.addEventListener(
+				'submit',
+				this.onSubmit);
 	}
 
 	onSubmit(event){
 		// block form from submitting
+		event.preventDefault();
 		// (which would refresh page)
 
 		// grab number values as strings
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
 
 		// cast strings to ints
 
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
+
 		// add numbers
 
+		const sum = add(num1, num2);
+
 		// output
+		alert(sum);
 	}
 }
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
