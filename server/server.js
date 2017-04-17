fcc7cf1c46845b70dd0c979bd3cfca6586f44e24
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-04-16 21:13:04 -0400

    edge case handling 404 and 500

--- server/server.js
+++ server/server.js
@@ -5,11 +5,19 @@ const url = require('url');
 const server = http.createServer(function(request, response) {
 
 	const urlPath = url.parse(request.url).pathname;
-	const filePath = `.${urlPath}`;
+	const filePath = `./client/${urlPath}`;
+
+	fs.exists(filePath, function(doesExist) {
+		if(!doesExist) {
+			response.statusCode = 404;
+			response.end(`Resource not found: "${urlPath}"`);
+		}
+	});
 
 	fs.readFile(filePath, (err, data) => {
 		if (err) {
-			response.end();
+			response.statusCode = 500;
+			response.end(`Server error: "${err}"`);
 		}
 		else {
 			response.end(data.toString('utf-8'));
