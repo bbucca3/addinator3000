707464ec70865a183a0cc885e42bd43bd2fd50af
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-04-16 20:56:53 -0400

    server parse request url pathname

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 const server = http.createServer(function(request, response) {
-	response.end('Hello world!');
+
+	const urlPath = url.parse(request.url).pathname;
+	const filePath = `.${urlPath}`;
+
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			response.end();
+		}
+		else {
+			response.end(data.toString('utf-8'));
+		}
+	});
+
 });
 
 server.listen(3000, function() {
